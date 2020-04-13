545d797fcdb3d7df5dbf920a472ab4004fb46f2c
Shawn <candle.magic@gmail.com>
2020-04-12 21:28:23 -0400

    config protocell, added RestaurantView classes, added constraints to data cell, add round corners

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by Nimbus IoT Solutions on 4/12/20.
+//  Copyright © 2020 Nimbus IoT Solutions. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
