ec14dd791984be8b278d6d33b9e46fba2863a446
Shawn <candle.magic@gmail.com>
2020-04-23 21:31:46 -0400

    Create view controllers "onboard" for the walkthrough sequence

---
+++ FoodPin/Controller/WalkthroughContentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WalkthroughContentViewController.swift
+//  FoodPin
+//
+//  Created by Nimbus IoT Solutions on 4/23/20.
+//  Copyright © 2020 Nimbus IoT Solutions. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
