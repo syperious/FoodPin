6b6f8bff71f0e9d5c19134c8e82ce5e4233f32b0
Shawn <candle.magic@gmail.com>
2020-04-26 01:03:16 -0400

    Discover tab set up using convenience API, data retrieval from cloudkit is functional.

---
+++ FoodPin/Controller/DiscoverTableViewController.swift
@@ -0,0 +1,84 @@
+//
+//  DiscoverTableViewController.swift
+//  FoodPin
+//
+//  Created by Nimbus IoT Solutions on 4/26/20.
+//  Copyright © 2020 Nimbus IoT Solutions. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class DiscoverTableViewController: UITableViewController {
+
+    var restaurants: [CKRecord] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.cellLayoutMarginsFollowReadableWidth = true
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        // Configure navigation bar appearance
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        if let customFont = UIFont(name: "Rubik-Medium", size: 40.0) {
+            navigationController?.navigationBar.largeTitleTextAttributes = [ NSAttributedString.Key.foregroundColor: UIColor(red: 231, green: 76, blue: 60), NSAttributedString.Key.font: customFont ]
+        }
+
+        fetchRecordsFromCloud()
+    }
+
+    
+    
+    func fetchRecordsFromCloud() {
+        // Fetch data using Convenience API
+        let cloudContainer = CKContainer.default()
+        let publicDatabase = cloudContainer.publicCloudDatabase
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: "Restaurant", predicate: predicate)
+        publicDatabase.perform(query, inZoneWith: nil, completionHandler: {
+            (results, error) -> Void in
+
+            if let error = error {
+                print(error)
+                return
+            }
+
+            if let results = results {
+                print("Completed the download of Restaurant data")
+                self.restaurants = results
+                DispatchQueue.main.async { //use async to free up the handler to run frontend refresh
+                    self.tableView.reloadData()
+                }
+            }
+        })
+    }
+    
+    //next 3 methods are used to display retrieved records
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurants.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DiscoverCell", for: indexPath)
+
+        // Configure the cell...
+        let restaurant = restaurants[indexPath.row]
+        cell.textLabel?.text = restaurant.object(forKey: "name") as? String
+
+        if let image = restaurant.object(forKey: "image"), let imageAsset = image as? CKAsset {
+
+            if let imageData = try? Data.init(contentsOf: imageAsset.fileURL!) {
+                cell.imageView?.image = UIImage(data: imageData)
+            }
+        }
+
+        return cell
+    }
+}
